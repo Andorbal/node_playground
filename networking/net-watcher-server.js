bee9fdeb770ec85d0fcd9141927cbc0babbe296d
Andrew Benz <andorbal@gmail.com>
2014-12-15 18:25:30 -0600

    Refactor server to handle multiple connections with a single watcher

--- networking/net-watcher-server.js
+++ networking/net-watcher-server.js
@@ -1,30 +1,66 @@
 "use strict";
 
-const fs = require('fs'),
-	  net = require('net'),
-	  filename = process.argv[2],
-	  server = net.createServer(function(connection) {
-	  	// reporting
-	  	console.log('Subscriber connected.');
-	  	connection.write(JSON.stringify({
-	  		type: 'watching',
-	  		file: filename
-	  	}) + '\n');
+const serverHandler = function(connection) {
+	let connections = new Array();
+	let watcher = null;
 
-	  	// watcher setup
-	  	let watcher = fs.watch(filename, function() {
-	  		connection.write(JSON.stringify({
+	// Handle when the specified file changes
+	function handleFileChange() {
+  		for (let i = 0; i < connections.length; i++) {
+  			connections[i].write(JSON.stringify({
 	  			type: 'changed',
 	  			file: filename,
 	  			timestamp: Date.now()
-	  		}) + '\n');
-	  	});
+	  		}) + '\n');	
+  		}
+  	}
 
-	  	connection.on('close', function() {
-	  		console.log('Subscriber disconnected.');
-	  		watcher.close();
-	  	});
-	  });
+	// Remove the specified connection from the connections array
+	function removeConnection(conn) {
+		let tempConnections = new Array();
+		let item = null;
+
+		while (item = connections.pop())
+		{
+			if (item !== conn)
+			{
+				tempConnections.push(item);
+			}
+		}
+
+		return tempConnections;		
+	}
+
+	if (connections.length == 0) {
+		console.log("Watching for changes...");
+	  	watcher = fs.watch(filename, handleFileChange);
+	}
+
+	connections.push(connection);
+
+	// reporting
+	console.log('Subscriber connected.');
+	connection.write(JSON.stringify({
+		type: 'watching',
+		file: filename
+	}) + '\n');
+	
+	connection.on('close', function() {
+		console.log('Subscriber disconnected.');
+
+		connections = removeConnection(connection);
+
+		if (connections.length == 0) {
+			console.log("Closing watcher...");
+			watcher.close();	
+		}
+	});
+};
+
+const fs = require('fs'),
+	  net = require('net'),
+	  filename = process.argv[2],
+	  server = net.createServer(serverHandler);
 
 if (!filename) {
 	throw Error("No target filename was specified.");
